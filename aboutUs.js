72c980d30df81c7064c30737d2f7ccc68b8d3531
Divyanka Chaudhari <divyanka.chaudhari25@gmail.com>
2021-03-08 22:50:42 +0530

    Minor update

--- aboutUs.js
+++ aboutUs.js
@@ -38,7 +38,7 @@ $("document").ready(function(){
     });
 
     $('#signupLink').on('click', function(){
-		window.location="https://covedindia.org/mentorsignup.html"
+		window.location="https://covedindia.org/mentorSignUp.html"
     });
 
     $('#contactusLink').on('click', function(){
@@ -67,11 +67,11 @@ $("document").ready(function(){
     });
 
     $('#studentsLinkS').on('click', function(){
-		window.location="https://covedindia.org/#studentsignup"
+		window.location="https://covedindia.org/mentorSignUp.html"
     });
 
     $('#mentorsLinkS').on('click', function(){
-		window.location="https://covedindia.org/#mentorsignup"
+		window.location="https://covedindia.org/mentorSignUp.html"
     });
 
     $('#contactusLinkS').on('click', function(){
